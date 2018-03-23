25af6f9cec71146ec9a4bf2f2b5f4f8f32442533
Sateek <sateeksuper@gmail.com>
2018-03-22 22:52:53 -0400

    First commit

---
+++ tab-script.js
@@ -0,0 +1,7 @@
+function tabHover(clsName) {
+    $(clsName).css("background-color", "rgba(255, 118, 0, 0.33)");
+}
+
+function tabHoverOut(className) {
+    $(className).css("background-color", "rgba(0, 0, 0, 0.64)")
+}
